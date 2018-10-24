29ef4d4657af9876bfdfb6fce46b27373b63567f
Narate Nantaburom <narate.nantaburom@thomsonreuters.com>
2018-10-24 09:38:11 +0700

    Added first code.

---
+++ ConsoleApp1/ConsoleApp1/Operation.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Operation
+    {
+        List<char> _operators = new List<char> { '/', '*', '-', '+' };
+        public Operation(string operationText)
+        {
+            var openParenthesisIndex = operationText.IndexOf("(");
+            if (openParenthesisIndex > -1)
+            {
+                var endParenthesisIndex = operationText.IndexOf(")");
+
+                var left = operationText.Substring(openParenthesisIndex + 1, endParenthesisIndex - 1);
+                
+
+                // Check if ended
+                if (operationText.Length - 1 == endParenthesisIndex)
+                {
+                    Left = new Operation(left);
+                    Value = Left.CalculateResult();
+                }
+                else
+                {
+                    var right = operationText.Substring(endParenthesisIndex + 2);
+                    Operator = operationText.Substring(endParenthesisIndex + 1, 1);
+                    OperationSide = OperationSideOfParent.NA;
+
+                    Left = new Operation(left);
+                    Right = new Operation(right);
+                }
+            }
+            else
+            {
+                HandleOperations(operationText);
+            }
+        }
+
+        private void HandleOperations(string operationText)
+        {
+            if (!operationText.Any(c => _operators.Contains(c)))
+            {
+                Value = double.Parse(operationText);
+            }
+            else
+            {
+                foreach (var op in _operators)
+                {
+                    var operatorIndex = operationText.IndexOf(op);
+                    if (operatorIndex > -1)
+                    {
+                        var left = operationText.Substring(0, operatorIndex);
+                        var right = operationText.Substring(operatorIndex + 1);
+                        Operator = operationText.Substring(operatorIndex, 1);
+                        OperationSide = OperationSideOfParent.NA;
+
+                        Left = new Operation(left);
+                        Right = new Operation(right);
+                    }
+                }
+            }
+        }
+
+        public OperationSideOfParent OperationSide { get; set; }
+
+        public Operation Left { get; set; }
+
+        public Operation Right { get; set; }
+
+        public string Operator { get; set; }
+
+        public double? Value { get; set; }
+
+        public double CalculateResult()
+        {
+            if (Value != null)
+                return Value.Value;
+
+            var leftValue = Left.CalculateResult();
+            var rightValue = Right.CalculateResult();
+
+            switch (Operator)
+            {
+                case "+": return leftValue + rightValue;
+                case "-": return leftValue - rightValue;
+                case "*": return leftValue * rightValue;
+                case "/": return leftValue / rightValue;
+            }
+
+            throw new InvalidConstraintException();
+        }
+    }
+
+    public enum OperationSideOfParent
+    {
+        NA,
+        Left,
+        Right
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exit = false;
+            while (!exit)
+            {
+                WriteRule();
+
+                var input = Console.ReadLine();
+                var result = Calculate(input);
+                Console.WriteLine(string.Format(@"Input:""{0}"" Result:{1}", input, result));
+                Console.WriteLine();
+                Console.WriteLine("Do you want to try again? If so, press Y, otherwist, press Enter");
+                ConsoleKeyInfo key = Console.ReadKey();
+                Console.WriteLine();
+                if (key.Key != ConsoleKey.Y)
+                {
+                    exit = true;
+                }
+            }
+        }
+
+        private static double Calculate(string input)
+        {
+            // Remove all spaces
+            input = input.Replace(" ", string.Empty);
+            var operation = new Operation(input);
+            var result = operation.CalculateResult();
+            return result;
+        }
+
+        private static void WriteRule()
+        {
+            Console.WriteLine("Please enter formula:");
+            Console.WriteLine();
+        }
+    }
+}
